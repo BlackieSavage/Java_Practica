1454dab7511d0b2de6fc2b7d4e95b89931c5ab31
Daniel Rosales <danelcan14@gmail.com>
2024-09-25 19:53:15 -0500

    Añadimos la parte del Else al condicional IF

--- condIf/src/condif/CondIf.java
+++ condIf/src/condif/CondIf.java
@@ -12,6 +12,10 @@ public class CondIf {
         {
             //Mostramos un mensaje si la condicion es verdadera
             System.out.print(num1 + " es menor que " + num2);
+        }  
+        else 
+        {
+            System.out.print(num1 + " no es menor que " + num2);
         }
     }
     
