ebe8292cac0cf34965d39d723397a6a79cf1ebb6
Daniel Rosales <danelcan14@gmail.com>
2024-09-25 19:32:19 -0500

    Agregamos los operadores que existen en Java

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -13,7 +13,55 @@ public class HolaMundo {
         char letra = 'a';
         String texto = "Hola amigos";
         long numeroGrande = 99999999;
-                
+               
+        //Operadores en Java
+        //Aritmeticos
+        int x = 10, y = 5, z, w = 20;
+        
+            //Suma
+        z = x + y;
+        
+            //Resta
+        z = x - y;
+        
+            //Multiplicación
+        z = x * y;
+        
+            //División
+        z = x / y;
+        
+        //Relacionales
+        boolean resultado;
+        
+            //Igualdad
+        resultado = x == y; 
+        
+            //Mayor que
+        resultado = x > y;
+        
+            //Menor que
+        resultado = x < y;
+        
+            //Mayor o igual que
+        resultado = x >= y;
+        
+            //Menor o igual que
+        resultado = x <= y;
+        
+            //Diferente que
+        resultado = x != y;
+        
+        //Condicionales
+        
+            //Operador Lógico AND
+        resultado = (x < y && x < w);
+        
+            //Operador Lógico OR
+        resultado = (x > y || x < w);
+        
+            //Operador Lógico NOT
+        resultado = !(x > y || x < w);
+             
     }
     
 }
