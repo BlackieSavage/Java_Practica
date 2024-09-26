ed384611e0d31875a4d97d174435c83cb6317ad3
Daniel Rosales <danelcan14@gmail.com>
2024-09-25 19:13:26 -0500

    Añadimos en el codigo los tipos de datos existentes en Java

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -4,7 +4,16 @@ public class HolaMundo {
 
     public static void main(String[] args) 
     {
-        System.out.print("Hola mundo");
+        System.out.print("Hola mundo"); //Mostramos un mensaje en la consola con la función print
+        
+        //Tipos de datos en Java
+        int entero = 10;
+        double decimal = 4.52;
+        boolean VOF = false;
+        char letra = 'a';
+        String texto = "Hola amigos";
+        long numeroGrande = 99999999;
+                
     }
     
 }
