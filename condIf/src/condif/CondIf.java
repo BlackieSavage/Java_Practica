f87291a2e460b8472a8dacee10357a6d5b15b452
Daniel Rosales <danelcan14@gmail.com>
2024-09-25 19:50:12 -0500

    Añadimos otra carpeta desarrollado el condicional IF

---
+++ condIf/src/condif/CondIf.java
@@ -0,0 +1,18 @@
+package condif;
+
+public class CondIf {
+
+    public static void main(String[] args) 
+    {
+        //Creamos las variables
+        int num1 = 5, num2 = 10;
+        
+        //Creamos el condicional
+        if (num1 < num2)
+        {
+            //Mostramos un mensaje si la condicion es verdadera
+            System.out.print(num1 + " es menor que " + num2);
+        }
+    }
+    
+}
